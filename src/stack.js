31a8a15d98daf33726b18ab42060e29d21e3a93e
elenadanchenkomn <elenadanchenkomn@gmail.com>
2022-10-17 23:13:55 +0200

    add function

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  push(element ) {
+   let result = [];
+   result.push(element)
     // remove line with error and write your code here
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    while(Stack.length){
+      Stack.pop();
+    }
     // remove line with error and write your code here
   }
 
   peek() {
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    Stack.peek();
   }
 }
 
